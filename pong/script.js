698b62dcb7ee0f6d044653a11a497357bdc44baf
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-01-25 16:14:48 +0100

    pong started

--- pong/script.js
+++ pong/script.js
@@ -8,12 +8,16 @@ const ctx = canvas.getContext("2d")
 // - variables globales
 let ballX = 50;
 let ballY = 50;
+let ballRadius = 20;
+let isBallGoingRight = true;
+let isBallGoingDown = true;
+let ballSpeed = 4;
 
 
 // - funciones
 const drawBall = () => {
   ctx.beginPath()
-  ctx.arc(ballX, ballY, 15, 0, 2 * Math.PI)
+  ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI)
   // ctx.strokeStyle = "magenta";
   // ctx.stroke();
   ctx.fillStyle = "white";
@@ -21,6 +25,39 @@ const drawBall = () => {
   ctx.closePath()
 }
 
+const moveBall = () => {
+  if (isBallGoingRight === true) {
+    ballX += ballSpeed
+  } else {
+    ballX -= ballSpeed
+  }
+  if (isBallGoingDown === true) {
+    ballY += ballSpeed
+  } else {
+    ballY -= ballSpeed
+  }
+}
+
+const colissionBallWall = () => {
+  // la posicion de la pelotita X deberia ser menor CW
+  if (ballX > canvas.width - ballRadius) {
+    console.log("pelotita colision pared derecha")
+    // la pelotita deberia empezar a moverse a la izquierda.  -- en la posX
+    isBallGoingRight = false;
+  }
+
+  if (ballY > canvas.height - ballRadius) {
+    isBallGoingDown = false;
+  }
+
+  if (ballX < 0 + ballRadius) {
+    isBallGoingRight = true;
+  }
+
+  if (ballY < 0 + ballRadius) {
+    isBallGoingDown = true;
+  }
+}
 
 
 // - funcion de recursion
@@ -31,8 +68,8 @@ const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   // 2. movimiento y acciones de todos los elementos
-  ballX++
-  ballY++
+  moveBall()
+  colissionBallWall()
 
   
   // 3. dibujado de todos los elementos
